92ef11208f38625afc311475c7a2460ccc78d124
Ber1276 <15134810353@163.com>
2024-07-14 11:40:56 +0800

    创作中心选择器完成

--- src/components/WangEditor.vue
+++ src/components/WangEditor.vue
@@ -5,6 +5,7 @@ import { Editor, Toolbar } from '@wangeditor/editor-for-vue'
 import '@wangeditor/editor/dist/css/style.css'
 import { PostSubmitArticle } from '@/api/user.js'
 
+
 // 编辑器实例，必须用 shallowRef
 const editorRef = shallowRef()
 const toolbarConfig = {}
@@ -18,6 +19,7 @@ onBeforeUnmount(() => {
 const handleCreated = (editor) => {
   editorRef.value = editor // 记录 editor 实例，重要！
 }
+
 
 // 内容 HTML
 const editorData = ref({
@@ -35,19 +37,13 @@ onMounted(() => {
   }, 1500)
 })
 
+
 //封面上传
 const token = { token: localStorage.getItem('token') }
 const fileList = ref([])
-const handleRemove = (file, uploadFiles) => {
-  console.log(file, uploadFiles)
-}
-const handlePreview = (uploadFile) => {
-  console.log(uploadFile)
-}
 const handleExceed = (files, uploadFiles) => {
   ElMessage.warning(
-    `The limit is 3, you selected ${files.length} files this time, add up to ${
-      files.length + uploadFiles.length
+    `The limit is 3, you selected ${files.length} files this time, add up to ${files.length + uploadFiles.length
     } totally`
   )
 }
@@ -58,78 +54,114 @@ const beforeRemove = (uploadFile) => {
   )
 }
 const handleSuccess = (response) => {
-  console.log(response)
   editorData.value.cover = response.data.url
 }
 
+// 类别选项
+const categoryOptions = [
+  {
+    value: '原创',
+    label: '原创'
+  },
+  {
+    value: '转载',
+    label: '转载'
+  },
+  {
+    value: '翻译',
+    label: '翻译'
+  }
+]
+
+// 可见性选项
+// const visibleOptions = [
+//   {
+//     value: '全部可见',
+//     label: '全部可见'
+//   },
+//   {
+//     value: '仅我可见',
+//     label: '仅我可见'
+//   },
+//   {
+//     value: '粉丝可见',
+//     label: '粉丝可见'
+//   }
+// ]
+
+
 //文章上传
 const handleSubmit = async (data, token) => {
-  const { data: res } = await PostSubmitArticle(data, token)
-  console.log(res)
+  try {
+    const res = await PostSubmitArticle(data, token)
+    console.log(res.message)
+    ElMessage({
+      type: 'success',
+      message: res.message
+    })
+  }
+  catch (err) {
+    console.log(err);
+  }
+
 }
+
+
 </script>
 
 <template>
   <div class="editor-demo mx-auto relative">
+    <!-- 编辑器实例开始 -->
+
+    <!-- 菜单栏开始 -->
     <Toolbar :editor="editorRef" :defaultConfig="toolbarConfig" />
-    <Editor
-      v-model="editorData.content"
-      :defaultConfig="editorConfig"
-      @onCreated="handleCreated"
-      class="editor-area"
-    />
+    <!-- 菜单栏结束 -->
+
+    <Editor v-model="editorData.content" :defaultConfig="editorConfig" @onCreated="handleCreated" class="editor-area" />
+
+    <!-- 编辑器示例结束 -->
+
+    <!-- 文章信息栏开始 -->
+
     <div class="w-full mt-4 grid-cols-4 grid-rows-1 grid gap-4">
+
       <div class="flex gap-2 mb-4 h-7">
         <span class="text-xl">标题</span>
-        <el-input
-          v-model="editorData.title"
-          style="width: 240px; height: 28px"
-          placeholder="your title"
-        />
+        <el-input v-model="editorData.title" style="width: 240px; height: 28px" placeholder="您的标题" />
       </div>
+
       <div class="flex gap-2 mb-4 h-7">
         <span class="text-xl">封面</span>
-        <el-upload
-          v-model:file-list="fileList"
-          class="upload-demo"
-          action="http://49.232.134.192:8081/article/upload/file"
-          :headers="token"
-          :on-preview="handlePreview"
-          :on-remove="handleRemove"
-          :before-remove="beforeRemove"
-          :limit="1"
-          :on-exceed="handleExceed"
-          :on-success="
-            (response) => {
-              return handleSuccess(response)
-            }
-          "
-        >
-          <el-button type="primary" style="height: 28px; width: 240px">Click to upload</el-button>
+        <el-upload v-model:file-list="fileList" class="upload-demo"
+          action="http://49.232.134.192:8081/article/upload/file" :headers="token" :before-remove="beforeRemove"
+          :limit="1" :on-exceed="handleExceed" :on-success="(response) => {
+            return handleSuccess(response)
+          }
+            ">
+          <el-button type="primary" style="height: 28px; width: 240px">上传封面</el-button>
         </el-upload>
       </div>
+
       <div class="flex gap-2 mb-4 h-7">
         <span class="text-xl">类别</span>
-        <el-input
-          v-model="editorData.category"
-          style="width: 240px; height: 28px"
-          placeholder="your category"
-        />
+        <el-select v-model="editorData.category" style="width: 240px; height: 28px" placeholder="文章类别">
+          <el-option v-for="item in categoryOptions" :key="item.label" :label="item.label"
+            :value="item.value"></el-option>
+        </el-select>
       </div>
+
       <div class="flex gap-2 mb-4 h-7">
         <span class="text-xl">摘要</span>
-        <el-input
-          v-model="editorData.summary"
-          style="width: 240px; height: 28px"
-          placeholder="your summary"
-        />
+        <el-input v-model="editorData.summary" style="width: 240px; height: 28px" placeholder="文章摘要" />
       </div>
+
     </div>
     <div>
       <el-button class="float-right" @click="handleSubmit(editorData, token.token)">
-        发表文章</el-button
-      >
+        发表文章</el-button>
     </div>
+
+    <!-- 文章信息栏结束 -->
   </div>
 </template>
 
