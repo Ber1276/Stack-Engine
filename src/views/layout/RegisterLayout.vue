04a89000a634c47c00435dae54a1b163edba8241
Ber1276 <15134810353@163.com>
2024-09-10 22:41:14 +0800

    feat: 重构了注册页面

--- src/views/layout/RegisterLayout.vue
+++ src/views/layout/RegisterLayout.vue
@@ -39,21 +39,26 @@ const RegisterFn = async (formEl) => {
 
 <template>
   <div class="form-container">
-    <el-form ref="ruleFormRef" class="card-body" :model="registerDetails" :rules="rules">
-      <el-form-item class="form-control" prop="username">
-        <el-input v-model="registerDetails.username" size="large" type="text" placeholder="username"
-          class="bg-white w-full my-2.5" />
-      </el-form-item>
-      <el-form-item class="form-control" prop="password">
-        <el-input v-model="registerDetails.password" type="password" size="large" placeholder="password"
-          class="bg-white w-full" />
-      </el-form-item>
-      <el-form-item class="form-control mt-6">
-        <el-button class="register-button w-full" @click="RegisterFn(ruleFormRef)">
-          Register
-        </el-button>
-      </el-form-item>
-    </el-form>
+    <div class="illustration">
+      <img src="/src/assets/img/register.svg" alt="logo">
+    </div>
+    <div class="register-form">
+      <el-form ref="ruleFormRef" class="form" :model="registerDetails" :rules="rules">
+        <h2>开始你的成长之旅</h2>
+        <el-form-item class="form-control" prop="username">
+          <el-input v-model="registerDetails.username" size="large" type="text" placeholder="username" class="" />
+        </el-form-item>
+        <el-form-item class="form-control" prop="password">
+          <el-input v-model="registerDetails.password" type="password" size="large" placeholder="password" class="" />
+        </el-form-item>
+        <el-form-item class="form-control">
+          <el-button class="register-button" @click="RegisterFn(ruleFormRef)">
+            Register
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+
   </div>
 
 </template>
@@ -67,5 +72,86 @@ const RegisterFn = async (formEl) => {
   --el-button-hover-text-color: white;
   --el-button-active-text-color: white;
   height: 46px;
+}
+
+.form-container {
+  background-color: #472FDB;
+  padding: 140px 280px;
+  display: flex;
+  font-family: Arial, sans-serif;
+  height: 100vh;
+}
+
+.illustration {
+  flex: 1;
+  border-top-left-radius: 8px;
+  border-bottom-left-radius: 8px;
+  background-color: #472FDB;
+
+  img {
+    width: 464px;
+    height: 480px;
+    object-fit: cover;
+  }
+}
+
+.register-form {
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  flex: 1;
+  box-sizing: border-box;
+  padding: 20px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #fff;
+
+  .form {
+    border-radius: 10px;
+    height: 60%;
+    margin: 0 40px;
+    background-color: #fff;
+  }
+}
+
+h2 {
+  color: #333;
+  margin-bottom: 1rem;
+}
+
+.form-group {
+  margin-bottom: 1rem;
+}
+
+input {
+  width: 100%;
+  padding: 0.5rem;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+}
+
+.sign-in-button {
+  width: 100%;
+  padding: 0.5rem;
+  background-color: #3949AB;
+  color: white;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.help-link {
+  text-align: right;
+  margin-top: 0.5rem;
+}
+
+.create-account {
+  margin-top: 2rem;
+  text-align: center;
+}
+
+a {
+  color: #3949AB;
+  text-decoration: none;
 }
 </style>
